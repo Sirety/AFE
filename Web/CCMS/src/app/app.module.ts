77eae0ce121b88977971b921992db8398e03b4d8
MathiasSchjoedt-Bavngaard <msb@iterator-it.dk>
2023-09-11 11:59:59 +0200

    Added base client template

--- Web/CCMS/src/app/app.module.ts
+++ Web/CCMS/src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HomeComponent,
     AboutComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
